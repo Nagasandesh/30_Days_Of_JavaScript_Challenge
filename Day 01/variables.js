03c3d608267f00ae0680e7d8cf7520a66b8374e0
Nagasandesh N <50497769+Nagasandesh@users.noreply.github.com>
2021-04-04 21:26:43 +0530

    Create variables.js

---
+++ Day 01/variables.js
@@ -0,0 +1,30 @@
+let  x = 'this is a string variable'
+const num = 45
+let alpha
+let beta = true
+let gamma = null
+
+let a
+let b
+let c
+let d
+
+let p = 45
+let q = 'empty'
+let r = 'years'
+let s = 'dance'
+
+const fname = 'Andy'
+const lname = 'Ramesh'
+const marital_status = 'abc'
+const country = 'XYZ'
+const age = 65 
+
+let myAge = '25'
+let yourAge = '30' 
+console.log(x,num, alpha, beta, gamma)
+// console.log(a,b,c,d)
+// console.log(p,q,r,s)
+// console.log(fname, lname, marital_status, country, age)
+console.log('I am '+ myAge +' years old')
+console.log('You are '+yourAge+ ' years old')
