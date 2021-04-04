7aabb7e8cee00dc9be16c33387ee063529f28782
Nagasandesh N <50497769+Nagasandesh@users.noreply.github.com>
2021-04-04 21:26:12 +0530

    Create datatypes.js
    It gives details about Datatypes in JS

---
+++ Day 01/datatypes.js
@@ -0,0 +1,11 @@
+let  xa = 'this is a string variable'
+const number = 45
+let ala
+let bheta = true
+let gama = null
+
+console.log(typeof(xa))
+console.log(typeof(number))
+console.log(typeof(ala))
+console.log(typeof(bheta))
+console.log(typeof(gama))
